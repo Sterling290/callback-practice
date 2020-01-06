fa4f2af0708f36e583b9dbac2cf7a2154a270036
Sterling290 <sterling.becvar@gmail.com>
2020-01-06 11:30:07 -0700

    finished

--- getMug.js
+++ getMug.js
@@ -1,8 +1,9 @@
 const pourCoffeeIntoMug = require('./pourCoffeeIntoMug');
+const drink = require('./drink');
 
 module.exports = function getMug() {
    setTimeout(() => {
-     console.log('getting mug from cabinet');
+     console.log('getting mug from cabinet'),pourCoffeeIntoMug(),drink();
 
    }, 1000);
 }
